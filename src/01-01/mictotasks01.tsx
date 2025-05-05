1ffd397b98cfd95d3a841153dc455926cfd2365b
Anton-013 <antonchuvashovoren@mail.ru>
2025-05-05 11:51:46 +0300

    setting

---
+++ src/01-01/mictotasks01.tsx
@@ -0,0 +1,24 @@
+import { Button } from "./Button";
+
+export const Mictotasks01 = () => {
+
+const Button1Foo = (subscriber: string, age: number, address: string) => {
+    console.log(subscriber, age, address);
+  }
+
+  const Button2Foo = (subscriber: string) => {
+    console.log(subscriber);
+  }
+
+  const Button3Foo = () => {
+    console.log("NoNeeee");    
+  }
+
+  return (
+    <div>
+      <Button name={'MyYouTubeChanel-1'} callBack={() => Button1Foo("im Vasya", 21, "Msk")} />
+      <Button name={'MyYouTubeChanel-2'} callBack={() => Button2Foo("im Ivan")} />
+      <Button name={"im Button"} callBack={Button3Foo}/>
+    </div>
+  )
+}
