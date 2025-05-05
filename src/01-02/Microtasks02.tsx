9928ec9704ddd53d6a154394cc4f18e8729f2e34
Anton-013 <antonchuvashovoren@mail.ru>
2025-05-06 01:07:51 +0300

    sprint 1 week 2

---
+++ src/01-02/Microtasks02.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+import { FullInput } from "./components/FullInput"
+
+
+export const Mictotasks02 = () => {
+
+    let [message, setMessage] = useState([
+        { message: 'message1' },
+        { message: 'message2' },
+        { message: 'message3' },
+    ])
+
+    const addMessage = (title: string) => {
+        let newMessage = { message: title }
+        setMessage([newMessage, ...message,])
+    }
+
+    return (
+        <>
+            <FullInput name={'+'} addMessage={addMessage} />
+            {message.map((el, index) => {
+                return (
+                    <div key={index} >{el.message}</div>
+                )
+            })}
+        </>
+    )
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Mictotasks01 } from './01-01/mictotasks01'
+import { Mictotasks01 } from './01-01/Mictotasks01'
+import { Mictotasks02 } from './01-02/Microtasks02'
 import './App.css'
 
 export const App = () => {
   return (
     <>
-      <Mictotasks01 />
+      {/* <Mictotasks01 /> */}
+      <Mictotasks02 />
     </>
   )
 }
